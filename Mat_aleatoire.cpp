d00a6ff54dcd0df074fea46303ec9655bb0812a9
thedoctor84 <ant984@free.fr>
2015-03-18 18:36:10 +0100

    creation des fichiers mat alea

---
+++ Mat_aleatoire.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <map>
+using namespace std;
+
+//matrice aléatoire avec un % de 0 (ex: mat_alea(70) donne 70% de 0 et le reste aléatoire)
+
+
+// Nb de Lignes et de col entre 5 et 10
+int rand_ligne_col(){
+    return rand()%(10) + 5;
+}
+
+
+
+map<pair<int,int>,int> generer_mat_aleatoire(int pourcentage_zero){
+   
+}
+
+
+int main() {
+
+	return 0;
+}
---
+++ Mat_aleatoire.h
@@ -0,0 +1,14 @@
+#ifndef _MAT_ALEA_
+#define _MAT_ALEA_
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <map>
+
+int rand_ligne_col();
+
+map generer_mat_aleatoire(int pourcentage_zero);
+
+
+#endif;
