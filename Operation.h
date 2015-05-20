552e9afefcb7599be966c23fa6bdbe7cc938af96
kangooroot <hoony@hotmail.fr>
2015-05-20 16:04:40 +0200

    salut j'ai modifié mes fichiers Operation.cpp et Operation.h

--- Operation.h
+++ Operation.h
@@ -1,8 +1,12 @@
 #include <map>
-
+using namespace std;
 #ifndef _OPERATION_H_
 #define _OPERATION_H_
 
-map<pair<int, int>, int> Addition(map<pair<int, int>, int>, map<pair<int, int>, int>);
+map<pair<int, int>, int> TRANSPOSEE(map<pair<int, int>, int>, int, int);
+map<pair<int, int>, int> ADDITION(map<pair<int, int>, int>, int, int, map<pair<int, int>, int>, int, int);
+map<pair<int, int>, int> SOUSTRACTION(map<pair<int, int>, int>, int, int, map<pair<int, int>, int>, int, int);
+map<pair<int, int>, int> MULTIPLICATION(map<pair<int, int>, int>, int, int, map<pair<int, int>, int>, int, int);
+
 
 #endif
