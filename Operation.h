dbbc0bb477b6060b31b88b300606e18c550b646c
kangooroot <hoony@hotmail.fr>
2015-05-29 19:03:04 +0200

    ajout commentaire operation.h

--- Operation.h
+++ Operation.h
@@ -1,12 +1,20 @@
-#include <map>
-using namespace std;
+/**
+ * \file		Operation.h
+ * \author		Rémi Drissi
+ * \version		4.0
+ * \date		29 mai 2015
+ * \brief		Fichier qui regroupe les différentes opérations applicables aux matrices
+ * \details		Contient les fonctions décrites dans Operation.cpp
+ */
+
 #ifndef _OPERATION_H_
-#define _OPERATION_H_
+	#define _OPERATION_H_
 
-map<pair<int, int>, int> TRANSPOSEE(map<pair<int, int>, int>, int, int);
-map<pair<int, int>, int> ADDITION(map<pair<int, int>, int>, int, int, map<pair<int, int>, int>, int, int);
-map<pair<int, int>, int> SOUSTRACTION(map<pair<int, int>, int>, int, int, map<pair<int, int>, int>, int, int);
-map<pair<int, int>, int> MULTIPLICATION(map<pair<int, int>, int>, int, int, map<pair<int, int>, int>, int, int);
-
+	#include <map>
+	using namespace std;
+	map<pair<int, int>, int> TRANSPOSEE(map<pair<int, int>, int>, int, int);
+	map<pair<int, int>, int> ADDITION(map<pair<int, int>, int>, int, int, map<pair<int, int>, int>, int, int);
+	map<pair<int, int>, int> SOUSTRACTION(map<pair<int, int>, int>, int, int, map<pair<int, int>, int>, int, int);
+	map<pair<int, int>, int> MULTIPLICATION(map<pair<int, int>, int>, int, int, map<pair<int, int>, int>, int, int);
 
 #endif
