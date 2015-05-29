4b1a021a8ec0e822ef03848db8cfb2dd8367863a
D-Masta <imran.loud@hotmail.fr>
2015-05-29 18:13:41 +0200

    nouveau main

--- main.cpp
+++ main.cpp
@@ -4,16 +4,36 @@
 #include "flots.h"
 #include "Mat_aleatoire.h"
 #define YELLOW "\033[01;33m"
+#define RED "\033[01;31m"
+#define BLUE "\033[01;34m"
+#define GREEN "\033[01;32m"
 using namespace std;
 
+/**
+ * \file      main.cpp
+ * \author    Gregoire
+ * \version   4.0
+ * \date       29 mai 2015
+ * \brief      Fonction principale que l'utilisateur va utiliser pour faire les opérations.
+ *
+ * \details    L'utilisateur choisit son opération puis décide s'il veut charger ou génerer une matrice aléatoirement pour faire l'opération, on lui propose ensuite s'il désire la sauvegarder.
+ */
+
+
+
+/**
+ * \brief      Fonction principale Main.
+ * \details    Effectue les opérations sur les matrices par l'utilisateur.
+ * \return     Valeur de retour du main.
+ */
 int main()
 {
     int ope;
-    cout<<"Bienvenu dans le programme d'interaction des MatrLib"<<endl;
+    cout<<"Bienvenue dans le programme d'interaction des MatrLib"<<endl;
     cout<<endl;
 
         cout<<"Veuillez selectionner une operation parmi :"<<endl;
-        cout<<"1)ADDITION   2)MULTIPLICATION   3)SOUSTRACTION   4)TRANSPOSEE"<<endl;
+        cout<<"1) ADDITION   2) MULTIPLICATION   3) SOUSTRACTION   4) TRANSPOSEE"<<endl;
         cin>>ope;
 
         if(ope == 1 || ope == 2 || ope == 3)
