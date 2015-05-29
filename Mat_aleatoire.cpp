51aaf2e18a16fa9a01c034558355e85a439c91fc
thedoctor84 <ant984@free.fr>
2015-05-29 16:35:38 +0200

    test correction matrice alea

--- Mat_aleatoire.cpp
+++ Mat_aleatoire.cpp
@@ -4,13 +4,13 @@
 #include "Mat_aleatoire.h"
 using namespace std;
 
-//matrice aléatoire avec un % de 0 (ex: mat_alea(70) donne 70% de 0 et le reste aléatoire)
 
-
-// Nb de Lignes et de col entre 5 et 10
-int rand_entre_min_et_max(int min, int max){
-    return rand()%(max-min) + min;
+int rand_entre_min_et_max(int min, int max)
+{
+    return rand()%(max-min+1) + min;
 }
+
+//matrice aléatoire avec un % de 0 (ex: mat_alea(70) donne 70% de 0 et le reste aléatoire)
 
 map<pair<int,int>,int> generer_mat_aleatoire(int pourcentage_zero,int& nbLigne,int& nbCol)
 
@@ -35,7 +35,7 @@ map<pair<int,int>,int> generer_mat_aleatoire(int pourcentage_zero,int& nbLigne,i
 
 	int nbDeNbNonNulsInseres = 1;
 
-	while(nbDeNbNonNulsInseres < nombresNonNuls)
+	while(nbDeNbNonNulsInseres <= nombresNonNuls)
 	{
 		nbAlea = rand_entre_min_et_max(1,100);
 		indLigAlea = rand_entre_min_et_max(0,nbLigne);
@@ -49,7 +49,9 @@ map<pair<int,int>,int> generer_mat_aleatoire(int pourcentage_zero,int& nbLigne,i
 
 		if (!existeDeja)
 		{
+			cout << "PASSAGE DANS INSERT !" << endl;
 			 MC.insert(pair<pair<int, int>, int> (make_pair(indLigAlea, indColAlea), nbAlea));
+			 cout << "Insertion a :" << indLigAlea<< "," << indColAlea << " de : " << nbAlea << endl;
 			 nbDeNbNonNulsInseres++;
 		}
 	}
