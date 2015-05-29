0c75386507aa9a18ca7daed9003cbf77d3750f4d
thedoctor84 <ant984@free.fr>
2015-05-29 18:35:32 +0200

    modif Mat_aleatoire : enregistrement de la matrice dans un fichier

--- Mat_aleatoire.cpp
+++ Mat_aleatoire.cpp
@@ -1,7 +1,20 @@
+/**
+ * \file      Mat_aleatoire.cpp
+ * \author    Antoine Letourneur
+ * \version   4.0
+ * \date      29 mai 2015
+ * \brief     Enregistre une matrice aleatoire dans un fichier
+ *
+ * \details   Sauvegarde une matrice de dim aleatoire avec une pourcentage de zeros donné 
+ *                  
+ */
+
+
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
 #include "Mat_aleatoire.h"
+#include "flots.h"
 using namespace std;
 
 
@@ -12,27 +25,30 @@ int rand_entre_min_et_max(int min, int max)
 
 //matrice aléatoire avec un % de 0 (ex: mat_alea(70) donne 70% de 0 et le reste aléatoire)
 
-map<pair<int,int>,int> generer_mat_aleatoire(int pourcentage_zero,int& nbLigne,int& nbCol)
+/**
+ * \brief	Fonction qui genere une matrice remplit aleatoirement avec une dimension aleatoire
+ * \details	Donne une matrice de dim aleatoire (entre 10 et 20), avec des valeurs aleatoires non nulles entre 1 et 100
+ * \param	pourcentage_zero Le pourcentage de 0 dans la matrice creuse (entier de 0 a 100)
+ */
+
+void generer_mat_aleatoire(int pourcentage_zero)
 
 {
 	srand (time(NULL));
-   	nbLigne = rand_entre_min_et_max(10,20);
-	nbCol = rand_entre_min_et_max(10,20);
-	cout << "Nombre de lignes : " << nbLigne << endl;
-	cout << "Nombre de Colonnes : " << nbCol << endl;
+   	int nbLigne = rand_entre_min_et_max(10,20);
+	int nbCol = rand_entre_min_et_max(10,20);
 	map<pair<int,int>,int> MC;
 	map <pair<int, int>, int>::iterator it;
 
 	int nombresNonNuls = (int)((100-pourcentage_zero)*nbLigne*nbCol/100);
-	cout << "Nombre de case non nulles : " << nombresNonNuls << endl;
 
 	int nbAlea = rand_entre_min_et_max(1,100);
 	int indLigAlea = rand_entre_min_et_max(0,nbLigne-1);
 	int indColAlea = rand_entre_min_et_max(0,nbCol-1);
+
 	bool existeDeja;
 
 	MC.insert(pair<pair<int, int>, int> (make_pair(indLigAlea, indColAlea), nbAlea));
-	cout << "Insertion a :" << indLigAlea<< "," << indColAlea << " de : " << nbAlea << endl;
 
 	int nbDeNbNonNulsInseres = 1;
 
@@ -50,27 +66,9 @@ map<pair<int,int>,int> generer_mat_aleatoire(int pourcentage_zero,int& nbLigne,i
 
 		if (!existeDeja)
 		{
-			cout << "PASSAGE DANS INSERT !" << endl;
 			 MC.insert(pair<pair<int, int>, int> (make_pair(indLigAlea, indColAlea), nbAlea));
-			 cout << "Insertion a :" << indLigAlea<< "," << indColAlea << " de : " << nbAlea << endl;
 			 nbDeNbNonNulsInseres++;
 		}
-		else
-		{
-			cout << "error !" << endl;
-		}
 	}
-
-	return MC;
+	Sauvegarde("Mat_aleatoire",MC,nbLigne,nbCol);
 }
-
-
-/*int main() 
-{
-	srand(time(NULL));
-	int test1(0),test2(0); 
-	map<pair<int,int>,int> MapTest = generer_mat_aleatoire(95,test1,test2);
-	//for(int i=0; i < 100; i++)	cout << "nb : "<< rand_ligne_col() << endl;
-
-	return 0;
-}*/
--- Mat_aleatoire.h
+++ Mat_aleatoire.h
@@ -10,6 +10,6 @@ using namespace std;
 
 int rand_entre_min_et_max(int min, int max);
 
-map<pair<int,int>,int> generer_mat_aleatoire(int pourcentage_zero,int& nbLigne,int& nbCol);
+void generer_mat_aleatoire(int pourcentage_zero);
 
 #endif
