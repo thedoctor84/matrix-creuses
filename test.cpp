c0281b0c0d024323436620aba5caa4ca53d26ac4
thedoctor84 <ant984@free.fr>
2015-05-31 20:09:38 +0200

    test.cpp vide en attendant celui d'imran

--- test.cpp
+++ test.cpp
@@ -1,10 +1,16 @@
-jaime les bananes omfggggggggggggggggg
+#include <iostream>
+#include "libmatcreuse.h"
 
+#define BASIC   "\033[00;00m"
+#define BOLD    "\033[01m" 
+#define RED     "\033[01;31m"
+#define GREEN   "\033[01;32m"
+#define YELLOW  "\033[01;33m"
+#define BLUE    "\033[01;34m"
 
+using namespace std;
 
-
-
-
-
-
-Yoloowwwww
+int main()
+{
+	
+}
