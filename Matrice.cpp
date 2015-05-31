d896d503a91c963463e063611404156d467589f9
Kangooroot <hoony@hotmail.fr>
2015-05-31 22:09:45 +0200

    modif Matrice.cpp

--- Matrice.cpp
+++ Matrice.cpp
@@ -11,11 +11,12 @@ using namespace std;
 Matrice::Matrice(string nom)
 {
 	fichier = nom;
+	int dimL = 0;
+	int dimC = 0;
+	int nbrElemNN = 0;
 	if(!Chargement(fichier, M, dimL, dimC, nbrElemNN))
 	{
-		dimL = 0;
-		dimC = 0;
-		nbrElemNN = 0;
+
 	}
 }
 
@@ -25,6 +26,7 @@ bool Matrice::Insertion(int x, int y, int valeur)
 {
 	if (x < 0 || y < 0 || x > dimL || y > dimC) return false;
 
+	map <pair<int, int>, int>::iterator it;
 	for (it = M.begin(); it != M.end(); it++)
 	{
 		if(it->first.first == x && it->first.second == y)
@@ -50,55 +52,58 @@ void Matrice::Set_dimL(int dim) {dimL = dim;}
 
 void Matrice::Set_dimC(int dim) {dimC = dim;}
 
+void Set_map(map<pair<int, int>, int> X) {M = X;}
+
 int  Matrice::Get_dimL() {return dimL;}
 
 int  Matrice::Get_dimC() {return dimC;}
 
+map<pair<int, int>, int>& Get_map() {return M;}
 
 Matrice& Addition(string nom, Matrice& A, Matrice& B)
 {
 	Matrice X(nom);
-	if(A->Get_dimL() != B->Get_dimL() || A->Get_dimC() != B->Get_dimC()) return NULL;
-	map<pair<int, int>, int> temp = ADDITION(A.M, A->Get_dimL(), A->Get_dimC(), B->M, B->Get_dimL(), B->Get_dimC());
-	X.M = temp;
-	X->Set_dimL(A->Get_dimL());
-	X->Set_dimC(B->Get_dimC());
-	Sauvegarde(nom, temp, X->Get_dimL(), X->Get_dimC());
+	if(A.Get_dimL() != B.Get_dimL() || A.Get_dimC() != B.Get_dimC()) return NULL;
+	map<pair<int, int>, int> temp = ADDITION(A.Get_map(), A.Get_dimL(), A.Get_dimC(), B.Get_map(), B.Get_dimL(), B.Get_dimC());
+	X.Set_map(temp);
+	X.Set_dimL(A.Get_dimL());
+	X.Set_dimC(B.Get_dimC());
+	Sauvegarde(nom, temp, X.Get_dimL(), X.Get_dimC());
 	return X;
 }
 
 Matrice& Soustraction(string nom, Matrice& A, Matrice& B)
 {
 	Matrice X(nom);
-	if(A->Get_dimL() != B->Get_dimL() || A->Get_dimC() != B->Get_dimC()) return NULL;
-	map<pair<int, int>, int> temp = SOUSTRACTION(A.M, A->Get_dimL(), A->Get_dimC(), B->M, B->Get_dimL(), B->Get_dimC());
-	X.M = temp;
-	X->Set_dimL(A->Get_dimL());
-	X->Set_dimC(B->Get_dimC());
-	Sauvegarde(nom, temp, X->Get_dimL(), X->Get_dimC());
+	if(A.Get_dimL() != B.Get_dimL() || A.Get_dimC() != B.Get_dimC()) return NULL;
+	map<pair<int, int>, int> temp = SOUSTRACTION(A.Get_map(), A.Get_dimL(), A.Get_dimC(), B.Get_map(), B.Get_dimL(), B.Get_dimC());
+	X.Set_map(temp);
+	X.Set_dimL(A.Get_dimL());
+	X.Set_dimC(B.Get_dimC());
+	Sauvegarde(nom, temp, X.Get_dimL(), X.Get_dimC());
 	return X;
 }
 
 Matrice& Multiplication(string nom, Matrice& A, Matrice& B)
 {
 	Matrice X(nom);
-	if(A->Get_dimC() != B->Get_dimL()) return NULL;
+	if(A.Get_dimC() != B.Get_dimL()) return NULL;
 
-	map<pair<int, int>, int> temp = MULTIPLICATION(A.M, A->Get_dimL(), A->Get_dimC(), B->M, B->Get_dimL(), B->Get_dimC());
-	X.M = temp;
-	X->Set_dimL(A->Get_dimL());
-	X->Set_dimC(B->Get_dimC());
-	Sauvegarde(nom, temp, X->Get_dimL(), X->Get_dimC());
+	map<pair<int, int>, int> temp = MULTIPLICATION(A.Get_map(), A.Get_dimL(), A.Get_dimC(), B.Get_map(), B.Get_dimL(), B.Get_dimC());
+	X.Set_map(temp);
+	X.Set_dimL(A.Get_dimL());
+	X.Set_dimC(B.Get_dimC());
+	Sauvegarde(nom, temp, X.Get_dimL(), X.Get_dimC());
 	return X;
 }
 
 Matrice& Transposee(string nom, Matrice& A)
 {
 	Matrice X(nom);
-	map<pair<int, int>, int> temp = TRANSPOSEE(A.M);
-	X.M = temp;
-	X->Set_dimL(A->Get_dimL());
-	X->Set_dimC(A->Get_dimC());
-	Sauvegarde(nom, temp, X->Get_dimL(), X->Get_dimC());
+	map<pair<int, int>, int> temp = TRANSPOSEE(A.Get_map());
+	X.Set_map(temp);
+	X.Set_dimL(A.Get_dimL());
+	X.Set_dimC(A.Get_dimC());
+	Sauvegarde(nom, temp, X.Get_dimL(), X.Get_dimC());
 	return X;
 }
--- Matrice.h
+++ Matrice.h
@@ -18,8 +18,10 @@ class Matrice
 		~Matrice();
 		void Set_dimL(int);
 		void Set_dimC(int);
+		void Set_map(map<pair<int, int>, int>);
 		int  Get_dimL();
 		int  Get_dimC();
+		map<pair<int, int>, int>& Get_map();
 		bool Insertion(int x, int y, int valeur);
 		bool Affichage();
 };
