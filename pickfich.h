9748d78afd587e2f411c6b4195a10c362c63d148
D-Masta <kizaru84@hotmail.fr>
2015-05-31 19:45:18 +0200

    new maj

--- pickfich.h
+++ pickfich.h
@@ -1,6 +1,10 @@
 #ifndef _PICKFICH_H_
 #define _PICKFICH_H_
 
+#include <string>
+
+using namespace std;
+
 bool file_exist(string& fichier);
 string recherchefich();
 void UpdateS(string nom_sauvegarde);
