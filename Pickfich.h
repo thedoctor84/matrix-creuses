b0a983e9927f80f9dcda6de6c54f71b2cad99ab7
D-Masta <kizaru84@hotmail.fr>
2015-06-01 03:22:41 +0200

    new maj

--- Pickfich.h
+++ Pickfich.h
@@ -5,6 +5,16 @@
 
 using namespace std;
 
+/**
+ * \file      pickfich.h
+ * \author    Imran
+ * \version   4.0
+ * \date       29 mai 2015
+ * \brief      Fichier pickfich.h.
+ *
+ * \details    Contient les prototypes des fonctions de verification de fichiers.
+ */
+
 bool file_exist(string& fichier);
 bool recherchefich(string nom_fichier);
 void UpdateS(string nom_sauvegarde);
