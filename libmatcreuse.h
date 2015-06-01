af2667305069ce851aaa9e90376c41c15da2f032
thedoctor84 <ant984@free.fr>
2015-06-01 03:37:41 +0200

    ajout doxy

--- libmatcreuse.h
+++ libmatcreuse.h
@@ -3,3 +3,12 @@
 #include "Operation.h"
 #include "Pickfich.h"
 #include "Matrice.h"
+
+/**
+ * \file      libmatcreuse.h
+ * \author    Antoine Letourneur
+ * \version   1.0
+ * \date      29 Mai 2015
+ * \brief     regroupe les points .h pour la librairie	    
+ *                 
+ */
